a8ff0e393bd19590cdd40eb30c53d24d9c73796b
webmasterdevlin <webmasterdevlin@gmail.com>
2018-09-14 19:04:06 +0800

    Fixed sign up 404 endpoint

--- OfficeApp/OfficeApp/Constants/URLs.cs
+++ OfficeApp/OfficeApp/Constants/URLs.cs
@@ -4,7 +4,7 @@
     {
         // Replace localhost with 10.0.2.2 on Android emulator
         public const string Department = "http://localhost:5000/api/departments/";
-        public const string Signup = "http://localhost:5000/api/register/signup/";
+        public const string Signup = "http://localhost:5000/api/register/";
         public const string Login = "http://localhost:5000/api/auth/login/";
 
 //        public const string Department = "http://10.0.2.2:5000/api/departments/";
