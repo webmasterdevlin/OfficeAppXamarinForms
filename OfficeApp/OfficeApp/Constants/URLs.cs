cef093fd086f874ca8603cf8807347839a439dec
webmasterdevlin <webmasterdevlin@gmail.com>
2018-09-15 07:57:57 +0800

    Changed to Android URL

--- OfficeApp/OfficeApp/Constants/URLs.cs
+++ OfficeApp/OfficeApp/Constants/URLs.cs
@@ -3,12 +3,12 @@
     public static class URLs
     {
         // Replace localhost with 10.0.2.2 on Android emulator
-        public const string Department = "http://localhost:5000/api/departments/";
-        public const string Signup = "http://localhost:5000/api/register/";
-        public const string Login = "http://localhost:5000/api/auth/login/";
+        //public const string Department = "http://localhost:5000/api/departments/";
+        //public const string Signup = "http://localhost:5000/api/register/";
+        //public const string Login = "http://localhost:5000/api/auth/login/";
 
-//        public const string Department = "http://10.0.2.2:5000/api/departments/";
-//        public const string Signup = "http://10.0.2.2:5000/api/register/signup/";
-//        public const string Login = "http://10.0.2.2:5000/api/auth/login/";
+        public const string Department = "http://10.0.2.2:5000/api/departments/";
+        public const string Signup = "http://10.0.2.2:5000/api/register/";
+        public const string Login = "http://10.0.2.2:5000/api/auth/login/";
     }
 }
