2b264648c185c1d1970b311dc482c391fa4643c3
Devlin Duldulao <webmasterdevlin@gmail.com>
2018-09-16 11:31:58 +0800

    Deleted commented urls

--- OfficeApp/OfficeApp/Constants/URLs.cs
+++ OfficeApp/OfficeApp/Constants/URLs.cs
@@ -4,15 +4,6 @@ namespace OfficeApp.Constants
 {
     public static class URLs
     {
-        //// Replace localhost with 10.0.2.2 on Android emulator
-        //public const string Department_iOS = "http://localhost:5000/api/departments/";
-        //public const string Signup_iOS = "http://localhost:5000/api/register/";
-        //public const string Login_iOS = "http://localhost:5000/api/auth/login/";
-
-        //public const string Department_Android = "http://10.0.2.2:5000/api/departments/";
-        //public const string Signup_Android = "http://10.0.2.2:5000/api/register/";
-        //public const string Login_Android = "http://10.0.2.2:5000/api/auth/login/";
-
         const string Android = "http://10.0.2.2:5000/";
         const string iOSandUWP = "http://localhost:5000/";
 
